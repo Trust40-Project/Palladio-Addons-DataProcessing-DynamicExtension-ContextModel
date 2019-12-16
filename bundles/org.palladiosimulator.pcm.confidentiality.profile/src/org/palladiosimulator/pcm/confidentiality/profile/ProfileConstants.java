05772543bc78ff124f0958fbd6d30d193cc68044
Maximilian Walter <maximilian.walter@kit.edu>
2019-12-16 16:55:18 +0100

    added profile constants

---
+++ bundles/org.palladiosimulator.pcm.confidentiality.profile/src/org/palladiosimulator/pcm/confidentiality/profile/ProfileConstants.java
@@ -0,0 +1,6 @@
+package org.palladiosimulator.pcm.confidentiality.profile;
+
+public class ProfileConstants {
+	public static final String POLICY_STRING = "policy";
+	public static final String PROFILE_NAME = "org.palladiosimulator.pcm.confidentiality.profile";
+}
