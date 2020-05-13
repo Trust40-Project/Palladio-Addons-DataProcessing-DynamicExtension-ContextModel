06f7a652a06fd6b17f0c150eea860fcb448e6d3b
Maximilian Walter <maximilian.walter@kit.edu>
2020-05-13 16:28:36 +0200

    added missing files

---
+++ bundles/org.palladiosimulator.pcm.confidentiality.context/src-gen/org/palladiosimulator/pcm/confidentiality/context/model/ContextAttribute.java
@@ -0,0 +1,18 @@
+/**
+ */
+package org.palladiosimulator.pcm.confidentiality.context.model;
+
+import org.palladiosimulator.pcm.core.entity.Entity;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Context Attribute</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.palladiosimulator.pcm.confidentiality.context.model.ModelPackage#getContextAttribute()
+ * @model abstract="true"
+ * @generated
+ */
+public interface ContextAttribute extends Entity {
+} // ContextAttribute
---
+++ bundles/org.palladiosimulator.pcm.confidentiality.context/src-gen/org/palladiosimulator/pcm/confidentiality/context/model/impl/ContextAttributeImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package org.palladiosimulator.pcm.confidentiality.context.model.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.palladiosimulator.pcm.confidentiality.context.model.ContextAttribute;
+import org.palladiosimulator.pcm.confidentiality.context.model.ModelPackage;
+
+import org.palladiosimulator.pcm.core.entity.impl.EntityImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Context Attribute</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public abstract class ContextAttributeImpl extends EntityImpl implements ContextAttribute {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected ContextAttributeImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return ModelPackage.Literals.CONTEXT_ATTRIBUTE;
+    }
+
+} //ContextAttributeImpl
